d82c872855c1d6ad76a7929d0941376151c0a0b9
ExpLang <616335643@qq.com>
2022-08-26 11:27:54 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '61404e9cc62a4df69da140c90e23a561',
+  rootNotionPageId: 'b1494259fee940d0b1136122bb58030a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '阿浪小破站❤️❤️｜ExpLang small site❤️❤️',
-  domain: 'https://www.notion.so/explang/61404e9cc62a4df69da140c90e23a561',
+  name: '阿浪小破站 ❤️｜ExpLang small site ❤️',
+  domain: 'https://www.notion.so/explang/Team-Home-b1494259fee940d0b1136122bb58030a',
   author: 'ExpLang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '❤️ 没有止步于此 一直在路上 ❤️',
 
   // social usernames (optional)
   twitter: 'ExpLang_Cn',
@@ -35,7 +35,9 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: true,
+  REDIS_HOST: 'redis-17262.c267.us-east-1-4.ec2.cloud.redislabs.com:17262',
+  REDIS_PASSWORD: 'P9tlI48MpYcNWTOtbAjPdy8gvbor9NAZ'
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
